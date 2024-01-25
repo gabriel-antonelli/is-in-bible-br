fa27f65116f0d124128e3bcc4ce0c102a1e4d187
gabriel-antonelli <gabrielantonelli25@gmail.com>
2024-01-25 15:45:08 -0300

    adding new logs and fast return to word_searcher_with_pebble

--- internal/modules/search/external/searcher/word_searcher_with_pebble.go
+++ internal/modules/search/external/searcher/word_searcher_with_pebble.go
@@ -17,15 +17,17 @@ func (w *WordSearcherWithPebble) Total(word string) int {
 	db := config.GetDB()
 	val, closer, err := db.Get([]byte(word))
 	if err != nil {
-		log.Fatalf("error getting key: %v", err)
+		log.Printf("error getting key '%s': %v", word, err)
+		return 0
 	}
 	err = closer.Close()
 	if err != nil {
-		log.Fatalf("error closing get: %v", err)
+		log.Printf("error closing get: %v", err)
 	}
 	intVal, err := strconv.Atoi(string(val))
 	if err != nil {
-		log.Fatalf("error converting string to int: %v", err)
+		log.Printf("error converting string to int: %v", err)
 	}
+	log.Printf("Total for word '%s': %d", word, intVal)
 	return intVal
 }
