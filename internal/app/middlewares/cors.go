e3d683e408c2799991ee4dac9c7c7a5037661575
gabriel-antonelli <gabrielantonelli25@gmail.com>
2024-02-07 13:22:32 -0300

    added new cors config

--- internal/app/middlewares/cors.go
+++ internal/app/middlewares/cors.go
@@ -1,11 +1,26 @@
 package middlewares
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func AddCorsMiddleWare(router *gin.Engine) *gin.Engine {
-	router.Use(cors.Default())
+	originNonRelease, originRelease := "http://localhost:3000", "https://is-in-bible-br-frontend.vercel.app"
+	router.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			if origin == originNonRelease && gin.Mode() != "release" {
+				return true
+			} else if origin == originRelease && gin.Mode() == "release" {
+				return true
+			}
+			return false
+		},
+		AllowMethods: []string{"GET", "OPTIONS"},
+		MaxAge:       12 * time.Hour,
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
 	return router
 }
